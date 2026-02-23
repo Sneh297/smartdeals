30e9163bcb8a5e4c3b6eaefd6cad4e6d9e4ddd6a
Sneh297 <snehdholia@gmail.com>
2026-02-23 23:38:18 +0530

    done

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
